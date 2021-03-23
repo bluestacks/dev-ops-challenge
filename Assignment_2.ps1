d67e1e54cf5d6b14bbe16858fe774bf98fe6454f
nehagoswami9595 <81223924+nehagoswami9595@users.noreply.github.com>
2021-03-23 14:26:04 +0530

    Add files via upload

---
+++ Assignment_2.ps1
@@ -0,0 +1,8 @@
+﻿
+
+
+$Line = (Get-Content -Path 'Desktop\logfile.log') ##sourcepath
+$ip = ($Line  |  Select-String -Pattern "\d{1,3}(\.\d{1,3}){3}" -AllMatches).Matches.Value 
+$ip | Group-Object | Sort-Object -Property Count -Descending | Select-Object -first 8 | ft Name,count
+
+
